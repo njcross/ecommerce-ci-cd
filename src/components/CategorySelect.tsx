fd5ec9ccb32f80432db0c59ce4cf3a30666a890a
Nick Cross <njcross1990@gmail.com>
2025-05-28 19:04:37 -0500

    stuff for firebase

--- src/components/CategorySelect.tsx
+++
@@ -1,1 +0,0 @@
-// Dropdown for selecting product category
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,22 @@
+// firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import type { Auth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1Enp1Da5KrOmLOSxviYvrWEIELCvgzMI",
+  authDomain: "myfirstproject-db322.firebaseapp.com",
+  projectId: "myfirstproject-db322",
+  storageBucket: "myfirstproject-db322.firebasestorage.app",
+  messagingSenderId: "544271676826",
+  appId: "1:544271676826:web:bbc955fe3da759fcaff623",
+  measurementId: "G-H4NGN4HX7Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth: Auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+export { auth };
